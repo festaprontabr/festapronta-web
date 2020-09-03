ad9eaf61fe8648f29ac8daac547716bca9e02e3a
Cairo Vieira <vieira.cairo@hotmail.com>
2020-09-02 22:40:42 -0300

    v.0.1.0

---
+++ prod/precache-manifest.54ab0170612605c318590ad481171689.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8c0c923717d5d66e27775aef16fe728",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c9e4641d67800bc1383",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6947118ff6a045d25a28",
+    "url": "/static/js/2.5a9301e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5a9301e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c9e4641d67800bc1383",
+    "url": "/static/js/main.a7502989.chunk.js"
+  },
+  {
+    "revision": "182c117975657a5ac11e",
+    "url": "/static/js/runtime-main.fa88c796.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
